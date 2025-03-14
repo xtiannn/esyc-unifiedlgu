9f71dcfcde084a7395e06a3e0782d35557fdd498
josevener <intern.josevener@gmail.com>
2025-03-14 19:07:22 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@ use App\Http\Controllers\AnnouncementController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 // Override Breeze’s login route
-Route::get('/login', [AuthenticatedSessionController::class, 'createOrAutoLogin']);
-Route::post('/login/store', [AuthenticatedSessionController::class, 'store']);
+Route::get('/login', [AuthenticatedSessionController::class, 'createOrAutoLogin'])->name('login');
+Route::post('/login/store', [AuthenticatedSessionController::class, 'store'])->name('login.store');
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 
 // Root route: Redirect based on auth status
